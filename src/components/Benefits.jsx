8b333980049ab35607f0d57b5ac872e68a41b176
ksaravanan8 <ksaravanan8@gatech.edu>
2023-10-07 21:27:57 -0400

    Added background image for benefits section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -12,43 +12,49 @@ const Benefits = (props) => {
   );
 
   return (
-    <div id={styles.benefits}>
-      <h1>With Degreement, you and your team will be able to</h1>
-      <motion.div
-        id={styles['benefits-holder']}
-        style={{ y: yBenefits, opacity: opacityBenefits }}
-      >
-        <section>
-          <img src={images.handshakeIcon} />
-          <h2>Close more deals than before</h2>
-          <p>by leveraging creative finance as a seamless and desired option</p>
-        </section>
-        <section>
-          <img src={images.partnerIcon} />
-          <h2>Attract more leads</h2>
-          <p>
-            by being known for faster closing times and cheaper transaction
-            costs, empowering both buyers and sellers
-          </p>
-        </section>
-        <section>
-          <img src={images.paymentsIcon} />
-          <h2>Become a one-stop-shop for all FSBO clients</h2>
-          <p>
-            providing the additional value of a Title company, escrow, and
-            notary service all in one and charge for it
-          </p>
-        </section>
-        <section>
-          <img src={images.leaderIcon} />
-          <h2>Become recognized as an innovative leader</h2>
-          <p>
-            in the industry by using cutting-edge technology to drastically
-            improve the transaction experience for clients
-          </p>
-        </section>
-      </motion.div>
-    </div>
+    <>
+      <div id={styles.benefits}>
+        <div id={styles['benefits-backdrop']}>
+          <h1>With Degreement, you and your team will be able to</h1>
+          <motion.div
+            id={styles['benefits-holder']}
+            style={{ y: yBenefits, opacity: opacityBenefits }}
+          >
+            <section>
+              <img src={images.handshakeIcon} />
+              <h2>Close more deals than before</h2>
+              <p>
+                by leveraging creative finance as a seamless and desired option
+              </p>
+            </section>
+            <section>
+              <img src={images.partnerIcon} />
+              <h2>Attract more leads</h2>
+              <p>
+                by being known for faster closing times and cheaper transaction
+                costs, empowering both buyers and sellers
+              </p>
+            </section>
+            <section>
+              <img src={images.paymentsIcon} />
+              <h2>Become a one-stop-shop for all FSBO clients</h2>
+              <p>
+                providing the additional value of a Title company, escrow, and
+                notary service all in one and charge for it
+              </p>
+            </section>
+            <section>
+              <img src={images.leaderIcon} />
+              <h2>Become recognized as an innovative leader</h2>
+              <p>
+                in the industry by using cutting-edge technology to drastically
+                improve the transaction experience for clients
+              </p>
+            </section>
+          </motion.div>
+        </div>
+      </div>
+    </>
   );
 };
 
